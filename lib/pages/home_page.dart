841bd86b2b81cfe9533855db7a3f39db3fb18d49
franco <withamadness@gmail.com>
2022-11-29 00:22:19 -0500

    libreria http-dart implementando

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  
+  //funcion para obtener data de los pokemon
+  getDataPokemon() async{
+    //Uri: engloba un url(mas extenso)
+    //URL: localizador de recurso en el servidor
+    Uri _uri = Uri.parse("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
+    //cualquier peticion a un servicio como tal a internet es asincrono
+    http.Response response = await http.get(_uri);
+    print(response.body);
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton( 
+          child: Text("Data"), onPressed: (() {
+            getDataPokemon();
+          }),
+        ),
+      ),
+    );
   }
 }
