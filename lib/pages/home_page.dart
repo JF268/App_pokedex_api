63bfced75f590bb42bd6c691da5c9a9972b6726b
franco <withamadness@gmail.com>
2022-11-29 12:26:03 -0500

    metodo getDataPokemon para obtener del servicio

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 
 class HomePage extends StatelessWidget {
-  
+  List pokemons = [];
   //funcion para obtener data de los pokemon
   getDataPokemon() async{
     //Uri: engloba un url(mas extenso)
@@ -11,12 +13,18 @@ class HomePage extends StatelessWidget {
     Uri _uri = Uri.parse("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
     //cualquier peticion a un servicio como tal a internet es asincrono
     http.Response response = await http.get(_uri);
-    print(response.body);
+    if(response.statusCode == 200){
+      Map<String, dynamic> myMap = json.decode(response.body);
+      pokemons = myMap["pokemon"];
+      print(pokemons);
+    }
 
   }
 
   @override
   Widget build(BuildContext context) {
+
+    getDataPokemon();
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView( 
